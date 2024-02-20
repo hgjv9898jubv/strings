240a5ad6a1902c420c0f1891f2d14d726fe1dfdd
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-20 13:35:55 +0300

    Update ELHYBA.py

--- ELHYBA.py
+++ ELHYBA.py
@@ -478,7 +478,7 @@ async def start_msg(app, message):
 
 
 @app.on_message(filters.text & filters.private)
-async def generator_and_about(app, m, user_id=None):
+async def generator_and_about(app, m, user_id: int):
     if m.text == "زانیاری دەرباری بۆت":
         text = ''
         text += "**🐍 زمانی پڕۆگرامینگ - پایثۆن**"
